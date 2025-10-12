74fa2e1c6cc4839ad2e30cd55b9f4a3f8fa59459
Ansh Aryan <ansharyan03@gmail.com>
2025-10-12 01:14:52 -0400

    Fix poetry env

--- backend/game/trivia.py
+++ backend/game/trivia.py
@@ -309,7 +309,7 @@ class TriviaGame:
     
     def get_snapshot(self) -> Dict[str, Any]:
         """Get a snapshot of the current game state."""
-        can_advance = len(self.current_answers) == 2 or (len(self.current_answers) == 1 and ((self.players["player1"].get_score() + self.players["player2"].get_score()) / POINTS_CORRECT) == (self.current_question_index + 1)))
+        can_advance = len(self.current_answers) == 2 or (len(self.current_answers) == 1 and ((self.players["player1"].get_score() + self.players["player2"].get_score()) / POINTS_CORRECT) == (self.current_question_index + 1))
         return {
             "game_id": str(self.id),
             "is_running": self.is_running,
